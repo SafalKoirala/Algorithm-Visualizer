91f1d63fe96a07afc5e4afc7fbd1b9131621bbac
SafalKoirala <safalkoirala92@gmail.com>
2022-10-02 17:10:10 +0545

    feat: add merge sort algorithm and animation

--- src/app/components/display-screen/display-screen.component.ts
+++ src/app/components/display-screen/display-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getMergeSortAnimations } from '../algorithms/merge-sort';
 
 @Component({
   selector: 'app-display-screen',
@@ -12,7 +13,14 @@ export class DisplayScreenComponent implements OnInit {
   public maxBars: number = 150;
   public barsCount: number[] = [];
 
+  algorithms = ['merge', 'heap', 'quick', 'bubble']
+
   NUMBER_OF_ARRAY_BARS = 100;
+  PRIMARY_COLOR = '#0080FF';
+  SECONDARY_COLOR = 'red';
+  ANIMATION_SPEED_MS = 10;
+  PIVOT_COLOR = "green";
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,7 +28,30 @@ export class DisplayScreenComponent implements OnInit {
   }
 
   public selectedAlgortihm($event: string): void {
-    this.value = $event;
+    switch ($event) {
+      case this.algorithms[0]:
+        this.value = this.algorithms[0];
+        this._mergeSort();
+        break;
+
+      case this.algorithms[1]:
+        //merge sort function
+        this.value = this.algorithms[1];
+        break;
+
+      case this.algorithms[2]:
+        //merge sort function
+        this.value = this.algorithms[2];
+        break;
+
+      case this.algorithms[3]:
+        //merge sort function
+        this.value = this.algorithms[3];
+        break;
+
+      default:
+        this.value = '';
+    }
   }
 
   public randomizeBar(): void {
@@ -35,4 +66,29 @@ export class DisplayScreenComponent implements OnInit {
   randomIntFromInterval(min: any, max: any) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+
+ private _mergeSort(): void{
+    const arrayBars = document.getElementsByClassName('bars');
+    let animations = getMergeSortAnimations(this.barsCount);
+  
+    for (let i = 0; i < animations.length; i++) {
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = <HTMLElement>arrayBars[barOneIdx];
+        const barTwoStyle = <HTMLElement>arrayBars[barTwoIdx];
+        const color = i % 3 === 0 ? this.SECONDARY_COLOR : this.PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.style.backgroundColor = color;
+          barTwoStyle.style.backgroundColor = color;
+        }, i * this.ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = <HTMLElement>arrayBars[barOneIdx];
+          barOneStyle.style.height = `${newHeight}px`;
+        }, i * this.ANIMATION_SPEED_MS);
+      }
+    }
+   }
 }
